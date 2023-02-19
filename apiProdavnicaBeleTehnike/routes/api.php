cf188ef7fabc62a47c777a37632d835a21bfb3c9
RadovanCikic <cikicsm@gmail.com>
2023-02-19 22:36:17 +0100

    zasticene api  rute

--- apiProdavnicaBeleTehnike/routes/api.php
+++ apiProdavnicaBeleTehnike/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProizvodController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,6 +18,32 @@ use Illuminate\Support\Facades\Route;
 Route::get("/proizvodi/{id}",[ProizvodController::class,'show']);
 Route::get("/proizvodi",[ProizvodController::class,'index']);
 
-Route::post("/proizvodi",[ProizvodController::class,'store']);
-Route::put("/proizvodi/{id}",[ProizvodController::class,'update']);
-Route::delete("/proizvodi/{id}",[ProizvodController::class,'destroy']);
+
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+
+Route::group(['middleware' => ['auth:sanctum']], function () {   
+    Route::get('/profiles', function (Request $request) {  
+        return auth()->user();
+    });
+     
+  //dodati za korpu nesto?
+  
+    Route::post('logout', [AuthController::class, 'logout']);  
+  
+});
+
+Route::middleware(['auth:sanctum','isAPIAdmin'])->group(function(){  
+
+    Route::get('/proveri', function(){
+        return response()->json(['message'=>'You are admin','status'=>200],200);
+    });
+
+    Route::post("/proizvodi",[ProizvodController::class,'store']);
+    Route::put("/proizvodi/{id}",[ProizvodController::class,'update']);
+    Route::delete("/proizvodi/{id}",[ProizvodController::class,'destroy']);
+    
+    Route::post('logout', [AuthController::class, 'logout']);  
+});
