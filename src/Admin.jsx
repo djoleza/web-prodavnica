f7977e31a80bda7b5a41a304e4c9a167bf726104
djoleza <ddjordjee@gmail.com>
2023-02-20 17:50:50 +0100

    pretraga proizvoda

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -37,11 +37,39 @@ function Admin({proizvodi,setPAzuriraj}) {
         
           });
       }
-  
+      function searchTable() {
+        // uzimamo vrednost iz polja za pretragu
+        var input = document.getElementById("searchInput").value.toUpperCase();
+        
+        // pronalazimo tabelu i sve redove u njoj
+        var table = document.getElementById("myTable");
+        var rows = table.getElementsByTagName("tr");
+        
+        // prolazimo kroz svaki red u tabeli
+        for (var i = 0; i < rows.length; i++) {
+          var cells = rows[i].getElementsByTagName("td");
+          var found = false;
+          // prolazimo kroz svaku ćeliju u trenutnom redu
+          for (var j = 0; j < cells.length; j++) {
+            var cell = cells[j];
+            // ako sadrži traženu vrednost, postavljamo found na true i prekidamo petlju
+            if (cell.innerHTML.toUpperCase().indexOf(input) > -1) {
+              found = true;
+              break;
+            }
+          }
+          // prikazujemo ili skrivamo red u zavisnosti od toga da li smo pronašli traženu vrednost
+          if (found) {
+            rows[i].style.display = "";
+          } else {
+            rows[i].style.display = "none";
+          }
+        }
+      }
     return (
-      <div className="containerTabela ">
+      <div className="containerTabela ">    
  
-
+                <input type="text" id="searchInput" onInput={searchTable} />
             <table>
             <thead>
                 <tr>
@@ -56,7 +84,7 @@ function Admin({proizvodi,setPAzuriraj}) {
 
                 </tr>
             </thead>
-            <tbody>
+            <tbody id="myTable">
                 {proizvodi.map((product) => (
                 <tr key={product.id}>
                     <td>{product.id}</td>
